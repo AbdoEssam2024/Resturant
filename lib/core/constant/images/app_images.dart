96649a0ca35b1a32b6d46ebd60da472bc20fa759
AbdoEssam2024 <eabdo2051@gmail.com>
2025-01-26 01:22:19 +0200

    Create Offers Section In Home Page

--- lib/core/constant/images/app_images.dart
+++ lib/core/constant/images/app_images.dart
@@ -3,11 +3,9 @@ class AppImages {
   static String onBoardOne = "${splashImagesPath}image_splash_one.png";
   static String onBoardTwo = "${splashImagesPath}image_splash_two.png";
   static String onBoardThree = "${splashImagesPath}image_splash_three.png";
-
   static String onBoardIconOne = "${splashImagesPath}Card icon.png";
   static String onBoardIconTwo = "${splashImagesPath}Transfer Document icon.png";
   static String onBoardIconThree = "${splashImagesPath}Deliver_Boy_Icon.png";
-
 
   static String homeImagesPath = "assets/images/home/";
   static String homeOne = "${homeImagesPath}home_image1.jpg";
@@ -16,7 +14,6 @@ class AppImages {
   static String homeFour = "${homeImagesPath}home_image4.png";
   static String homeFive = "${homeImagesPath}home_image5.png";
   static String homeSix = "${homeImagesPath}home_image6.png";
-  static String pizza = "${homeImagesPath}pizza.png";
 
   static String lottiesPath = "assets/lottie/";
   static String loading = "${lottiesPath}loading.json";
